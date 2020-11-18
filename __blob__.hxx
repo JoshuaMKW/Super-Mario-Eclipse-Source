a239883aeff6ed937bd31cbe05cee5b5e914c965
JoshuaMKW <joshuamkw2002@gmail.com>
2020-11-18 03:12:37 -0600

    mr regolph

--- __blob__.hxx
+++ __blob__.hxx
@@ -12672,7 +12672,7 @@ public:
             float mWaterJumpHeightDifMax;
         } DefaultAttrs; //0x0024
 
-        u8 _mLastFrameYoshiState; //0x0054
+        bool _mFirstParamsDone; //0x0054
         u8 _00;             //0x0055
         u16 _01;            //0x0056
     };
@@ -12847,59 +12847,36 @@ public:
             }
             MarioParamsFile *params = this->mCustomInfo->mParams;
 
-            if (this->mYoshi)
-            {
-                if (this->mYoshi->mState == TYoshi::STATE::MOUNTED && this->mCustomInfo->_mLastFrameYoshiState != TYoshi::STATE::MOUNTED)
-                {
-                    this->mGravity = this->mCustomInfo->DefaultAttrs.mGravity;
-                    this->mCustomInfo->mTerminalVelocity = -75;
-                    this->mMaxFallNoDamage *= this->mCustomInfo->DefaultAttrs.mMaxFallNoDamage;
-                    this->mCustomInfo->mMaxJumps = params->Attributes.mJumpCount;
-                    this->mOceanOfs = this->mCustomInfo->DefaultAttrs.mOceanOfs;
-                    this->mWaterJumpHeightDifMax = this->mCustomInfo->DefaultAttrs.mWaterJumpHeightDifMax;
-                
-                    this->mBaseBounceSpeed1 = params->Attributes.mBaseBounce1Multi;
-                    this->mBaseBounceSpeed2 = params->Attributes.mBaseBounce2Multi;
-                    this->mBaseBounceSpeed3 = params->Attributes.mBaseBounce3Multi;
-                }
-                else if (this->mYoshi->mState != TYoshi::STATE::MOUNTED && this->mCustomInfo->_mLastFrameYoshiState == TYoshi::STATE::MOUNTED)
-                {
-                    this->mGravity *= params->Attributes.mGravityMulti;
-                    this->mCustomInfo->mTerminalVelocity = -75 * params->Attributes.mGravityMulti;
-                    this->mMaxFallNoDamage *= params->Attributes.mMaxFallNoDamageMulti;
-                    this->mCustomInfo->mMaxJumps = params->Attributes.mJumpCount;
-
-                    this->mSize.x *= baseParams->Attributes.mSizeMultiplier;
-                    this->mSize.y *= baseParams->Attributes.mSizeMultiplier;
-                    this->mSize.z *= baseParams->Attributes.mSizeMultiplier;
-                    this->mModelData->mModel->mSizeMultiplier.x *= baseParams->Attributes.mSizeMultiplier;
-                    this->mModelData->mModel->mSizeMultiplier.y *= baseParams->Attributes.mSizeMultiplier;
-                    this->mModelData->mModel->mSizeMultiplier.z *= baseParams->Attributes.mSizeMultiplier;
-                    this->mOceanOfs *= baseParams->Attributes.mSizeMultiplier;
-                    this->mWaterJumpHeightDifMax *= baseParams->Attributes.mSizeMultiplier;
-
-                    this->mWaterHealthDrainSpd /= params->Attributes.mWaterHealthMultiplier;
-                    this->mWaterHealthScubaDrainSpd /= params->Attributes.mWaterHealthMultiplier;
-                    this->mBaseBounceSpeed1 *= params->Attributes.mBaseBounce1Multi;
-                    this->mBaseBounceSpeed2 *= params->Attributes.mBaseBounce2Multi;
-                    this->mBaseBounceSpeed3 *= params->Attributes.mBaseBounce3Multi;
-                    this->mThrowPower *= params->Attributes.mThrowPowerMultiplier;
-                }
-                this->mCustomInfo->_mLastFrameYoshiState = this->mYoshi->mState;
-            }
-            else
+            this->mGravity = this->mCustomInfo->DefaultAttrs.mGravity;
+            this->mCustomInfo->mTerminalVelocity = -75;
+            this->mMaxFallNoDamage = this->mCustomInfo->DefaultAttrs.mMaxFallNoDamage;
+            this->mCustomInfo->mMaxJumps = 1;
+            this->mOceanOfs = this->mCustomInfo->DefaultAttrs.mOceanOfs;
+            this->mWaterJumpHeightDifMax = this->mCustomInfo->DefaultAttrs.mWaterJumpHeightDifMax;
+        
+            this->mBaseBounceSpeed1 = params->Attributes.mBaseBounce1Multi;
+            this->mBaseBounceSpeed2 = params->Attributes.mBaseBounce2Multi;
+            this->mBaseBounceSpeed3 = params->Attributes.mBaseBounce3Multi;
+            
+            if (!this->mYoshi || this->mYoshi->mState != TYoshi::STATE::MOUNTED)
             {
                 this->mGravity *= params->Attributes.mGravityMulti;
                 this->mCustomInfo->mTerminalVelocity = -75 * params->Attributes.mGravityMulti;
                 this->mMaxFallNoDamage *= params->Attributes.mMaxFallNoDamageMulti;
                 this->mCustomInfo->mMaxJumps = params->Attributes.mJumpCount;
 
-                this->mSize.x *= baseParams->Attributes.mSizeMultiplier;
-                this->mSize.y *= baseParams->Attributes.mSizeMultiplier;
-                this->mSize.z *= baseParams->Attributes.mSizeMultiplier;
-                this->mModelData->mModel->mSizeMultiplier.x *= baseParams->Attributes.mSizeMultiplier;
-                this->mModelData->mModel->mSizeMultiplier.y *= baseParams->Attributes.mSizeMultiplier;
-                this->mModelData->mModel->mSizeMultiplier.z *= baseParams->Attributes.mSizeMultiplier;
+                if (!this->mCustomInfo->_mFirstParamsDone)
+                {
+                    this->mSize.x *= baseParams->Attributes.mSizeMultiplier;
+                    this->mSize.y *= baseParams->Attributes.mSizeMultiplier;
+                    this->mSize.z *= baseParams->Attributes.mSizeMultiplier;
+                    this->mModelData->mModel->mSizeMultiplier.x *= baseParams->Attributes.mSizeMultiplier;
+                    this->mModelData->mModel->mSizeMultiplier.y *= baseParams->Attributes.mSizeMultiplier;
+                    this->mModelData->mModel->mSizeMultiplier.z *= baseParams->Attributes.mSizeMultiplier;
+                
+                    this->mCustomInfo->_mFirstParamsDone = true;
+                }
+
                 this->mOceanOfs *= baseParams->Attributes.mSizeMultiplier;
                 this->mWaterJumpHeightDifMax *= baseParams->Attributes.mSizeMultiplier;
 
